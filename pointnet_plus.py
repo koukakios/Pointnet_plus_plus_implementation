e5178224d326ddb39d2e3d2f923ecd338fada326
Kostas Koukakis <kkoukakis@tudelft.nl>
2026-05-22 10:18:52 +0200

    feat: added the segmentation head, time to test the shapes

--- pointnet_plus.py
+++ pointnet_plus.py
@@ -18,6 +18,13 @@ class Pointnet_plus(torch.nn.Module):
         dims_sa2_2nd_mlp = [dims_sa2_1st_mlp[-1], 64, 128, 1024]
         self.sa2 = SA(dims_sa2_1st_mlp, dims_sa2_2nd_mlp, ratio=0.25, k=32)
 
+        """
+        The basis of the pointnet++ stops here. After this we split into segmentation and classification
+        """
+
+        """
+        classification
+        """
         #final pointnet in the end on all points practically so no sampling
         dims_pointnet_1st_mlp = [dims_sa2_2nd_mlp[-1], 64, 64]
         dims_pointnet_2nd_mlp = [dims_pointnet_1st_mlp[-1], 64, 128, 1024]
@@ -32,14 +39,35 @@ class Pointnet_plus(torch.nn.Module):
             torch.nn.Linear(64, num_classes)
         )
 
+        """
+        Segmentation
+        """
+
     def forward(self, x):
         # x: (B, N, dim_in) in this case dim_in is x, y, z, etc extra feats
 
         x_3d = x[:, :, :3]
         x_features = x
+
+        #used later for skip connections to segmentation
+        x_3d_1st_layer = torch.clone(x_3d)
+        x_features_1st_layer = torch.clone(x_features)
+
         x_3d, x_features = self.sa1(x_3d, x_features)   # (B, M1, dims_sa1_2nd_mlp[-1])
+
+        # used later for skip connections to segmentation
+        x_3d_2nd_layer = torch.clone(x_3d)
+        x_features_2nd_layer = torch.clone(x_features)
+
         x_3d, x_features = self.sa2(x_3d, x_features)   # (B, M2, dims_sa2_2nd_mlp[-1])
 
+        """
+        The basis of the pointnet++ stops here. After this we split into segmentation and classification
+        """
+
+        """
+        classification
+        
         #from here onwards we dont need the x_3d as we aint doin any sampling.
         #we take all the features given by the last SetAbstraction and put them in pointnet
 
@@ -50,5 +78,62 @@ class Pointnet_plus(torch.nn.Module):
         x = self.pointnet(x_expanded)   # (B, 1, dims_pointnet_2nd_mlp[-1])
 
         x = self.classifier(x[:, 0, :])  # (B, num_classes)
+        """
+
+        """
+        segmentation
+        """
+        #interpolate
+        #x_3d (B, M, 3)
+        #x_features (B, M, dims_sa2_2nd_mlp[-1] + dims of x_features_2nd_layer)
+        x_3d, x_features = self.interpolate(x_3d, x_features, x_3d_2nd_layer, x_features_2nd_layer)
+
+        #unit pointnet
+        x_features = self.pointnet.mlp_1(x_features)
+        x_features = self.pointnet.mlp_2(x_features)
+
+        #interpolate
+        x_3d, x_features = self.interpolate(x_3d, x_features, x_3d_1st_layer, x_features_1st_layer)
+
+        # unit pointnet
+        x_features = self.pointnet.mlp_1(x_features)
+        x_features = self.pointnet.mlp_2(x_features)
 
         return x
+
+    def interpolate(self, x_3d_downsampled, x_features_downsampled,
+                    x_3d_original, x_features_original):
+        """
+        x_3d_downsampled:       (B, N, 3)
+        x_features_downsampled: (B, N, D)
+        x_3d_original:          (B, M, 3)
+        x_features_original:    (B, M, L)
+
+        returns:                (B, M, D + L)
+        """
+
+        # Distance from each original point to each downsampled point
+        dist = torch.cdist(x_3d_original, x_3d_downsampled)  # (B, M, N)
+
+        # For each original point, get 3 nearest downsampled points
+        dist, idx = dist.topk(k=3, dim=-1, largest=False)  # (B, M, 3)
+
+        # Compute inverse-distance weights
+        weights = 1.0 / (dist + 1e-8)  # (B, M, 3)
+        weights = weights / weights.sum(dim=-1, keepdim=True)  # (B, M, 3)
+
+        # Take the features of those 3 nearest points
+        neighbor_feats = x_features_downsampled.gather(
+            dim=1,
+            index=idx.reshape(idx.shape[0], -1).unsqueeze(-1).expand(-1, -1, x_features_downsampled.shape[-1])
+        )
+
+        neighbor_feats = neighbor_feats.reshape(
+            idx.shape[0], idx.shape[1], idx.shape[2], x_features_downsampled.shape[-1]
+        )  # (B, M, 3, D)
+
+        # Weighted average of the 3 features
+        interpolated = (neighbor_feats * weights.unsqueeze(-1)).sum(dim=2)  # (B, M, D)
+
+        # Add skip connection from original features and return
+        return x_3d_original, torch.cat([interpolated, x_features_original], dim=-1)  # (B, M, D + L)
